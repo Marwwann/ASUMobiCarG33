8b811b546c55f921a1c0b9733d703ed6c5ecc37a
mariamrefaat <36284031+mariamrefaat@users.noreply.github.com>
2018-02-20 20:29:09 +0200

    Add files via upload

---
+++ fade99.ino
@@ -0,0 +1,20 @@
+//This example shows how to fade an LED on pin 9 using the analogWrite()fn.
+  
+  
+int led = 9;           
+int brightness = 0;   
+int fade = 5;    
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+
+void loop() {
+  analogWrite(led, brightness);
+ brightness = brightness + fade;
+ if (brightness <= 0 || brightness >= 255) {
+    fade = -fade;
+  }
+   delay(60);
+}
