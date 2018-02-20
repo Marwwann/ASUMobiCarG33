6b775dc88bea169d68168c96f1d516bfa402234a
mariam-khaled <36280726+mariam-khaled@users.noreply.github.com>
2018-02-20 18:52:45 +0200

    Add files via upload

---
+++ traffic_light_circuit/traffic_light_circuit.ino
@@ -0,0 +1,33 @@
+int red=4;
+int yellow=5;
+int green=6;
+
+void setup() {
+  
+  pinMode(red,OUTPUT);
+  pinMode(yellow,OUTPUT);
+  pinMode(green,OUTPUT);
+  
+
+}
+
+void loop() {
+ digitalWrite(green,HIGH);
+ delay(20000);
+ digitalWrite(green,LOW);
+ digitalWrite(yellow,HIGH);
+ delay(1000);
+ digitalWrite(yellow,LOW);
+ delay(1000);
+ digitalWrite(yellow,HIGH);
+ delay(1000);
+ digitalWrite(yellow,LOW);
+ delay(1000);
+ digitalWrite(yellow,HIGH);
+ delay(1000);
+ digitalWrite(yellow,LOW);
+ delay(1000);
+ digitalWrite(red,HIGH);
+ delay(20000);
+ digitalWrite(red,LOW);
+}
