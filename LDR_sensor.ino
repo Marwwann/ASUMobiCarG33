24eac069561d51a83a55116323361315e14b9a46
mariam-khaled <36280726+mariam-khaled@users.noreply.github.com>
2018-02-22 12:06:25 +0200

    Add files via upload

---
+++ LDR_sensor.ino
@@ -0,0 +1,21 @@
+int led=7;
+int ldr=A0;
+int ldr_state;
+
+void setup() {
+ pinMode(led,OUTPUT);
+ pinMode(ldr,INPUT);
+
+}
+
+void loop() {
+  ldr_state=analogRead(ldr);
+ if(ldr_state<=300)
+ {
+   digitalWrite(led,HIGH);
+  }
+  else{
+    digitalWrite(led,LOW);
+    }
+
+}
