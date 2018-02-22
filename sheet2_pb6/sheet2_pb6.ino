7d4b897a6fe33106634a1144a2301780b3dbc763
mariam-khaled <36280726+mariam-khaled@users.noreply.github.com>
2018-02-22 23:17:16 +0200

    Add files via upload

---
+++ sheet2_pb6/sheet2_pb6.ino
@@ -0,0 +1,44 @@
+int ledA =5;
+int a=A0; //potentiometer to control ledA
+int ledB =6;
+int b=A1;  //potentiometer to control ledB
+int ledC =7;
+int c=A2;  //potentiometer to control ledC
+int potA,potB,potC;
+void setup() {
+  pinMode(a,INPUT);
+  pinMode(b,INPUT);
+  pinMode(c,INPUT);
+  pinMode(ledA,OUTPUT);
+  pinMode(ledB,OUTPUT);
+  pinMode(ledC,OUTPUT);
+
+}
+
+void loop() {
+ potA=analogRead(a);
+ potB=analogRead(b);
+ potC=analogRead(c);
+ if (potA>potB)
+ {
+   if (potB>potC)
+   {
+     digitalWrite(ledA,HIGH);
+     digitalWrite(ledB,LOW);
+     digitalWrite(ledC,LOW);
+    }
+  }
+  else if(potB>potC)
+  {
+    digitalWrite(ledB,HIGH);
+     digitalWrite(ledA,LOW);
+     digitalWrite(ledC,LOW);
+    }
+     else 
+  {
+    digitalWrite(ledC,HIGH);
+     digitalWrite(ledA,LOW);
+     digitalWrite(ledC,LOW);
+    }
+    
+}
