6ff76acdc0db4ca18513d4e42e3df466c6c20b11
MariamSafwat <36268243+MariamSafwat@users.noreply.github.com>
2018-02-27 21:38:43 +0200

    Add files via upload

---
+++ moveLeftFn/moveLeftFn.ino
@@ -0,0 +1,26 @@
+int LM1=4;
+int LM2=5; // motor1 ground
+int RM1=6;
+int RM2=7; // motor2 ground
+
+void left(){
+  digitalWrite(LM1,HIGH);
+  digitalWrite(LM2,LOW);
+  digitalWrite(RM1,LOW);
+  digitalWrite(RM2,LOW);
+
+}
+}
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LM1,OUTPUT);
+  pinMode(LM2,OUTPUT);
+  pinMode(RM1,OUTPUT);
+  pinMode(RM2,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
