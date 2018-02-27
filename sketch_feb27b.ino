548df405ee7162185b38fbdc962db3ac41046082
marwaabdelmenam <36280243+marwaabdelmenam@users.noreply.github.com>
2018-02-27 22:30:56 +0200

    Add files via upload

---
+++ sketch_feb27b.ino
@@ -0,0 +1,15 @@
+ int  ledpin =7 ;
+ void setup() {
+pinMode  (ledpin,OUTPUT );
+  // initialize digital pin 7 as an output .
+
+}
+
+void loop() {
+  digitalWrite (ledpin , HIGH ); // turn the led on 
+   delay (1000);  // wait for a second 
+   digitalWrite ( ledpin, LOW ); // turn the led off 
+   delay (1000) ; // wait for a second  
+   
+
+}
