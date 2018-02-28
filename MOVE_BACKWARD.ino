d5879d69b2abd66e83526135545131bdb2bc0750
marwaabdelmenam <36280243+marwaabdelmenam@users.noreply.github.com>
2018-02-28 06:53:09 +0200

    Add files via upload

---
+++ MOVE_BACKWARD.ino
@@ -0,0 +1,26 @@
+   int LM1=4 ;
+   int LM2 =5 ;
+   int RM1=6  ;
+   int RM2 = 7 ;
+void setup() {
+  pinMode (LM1 , OUTPUT );
+   pinMode (LM2 , OUTPUT );
+    pinMode (RM1 , OUTPUT );
+     pinMode (RM2 , OUTPUT );
+
+}
+
+void backward ()
+{
+  digitalWrite(LM1,LOW);
+  digitalWrite(LM2,HIGH);
+  digitalWrite(RM1,LOW);
+  digitalWrite(RM2,HIGH);
+
+
+}
+void loop ()
+{
+  
+}
+
