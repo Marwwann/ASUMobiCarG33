dd998ed3f5086c3f790b011f9cf8f7f3fdbb39ba
Mari-amAshraf <36281853+Mari-amAshraf@users.noreply.github.com>
2018-03-05 21:34:37 +0200

    Add files via upload

---
+++ leftbackward.ino
@@ -0,0 +1,20 @@
+int IN_1=4; //left motor
+int IN_2=5; //left motor
+int IN_3=6; //right motor
+int IN_4=7; //right motor
+void setup() {
+ pinMode(IN_1,OUTPUT);
+ pinMode(IN_2,OUTPUT);
+ pinMode(IN_3,OUTPUT);
+ pinMode(IN_4,OUTPUT);
+}
+ void leftbackward()
+{
+  digitalWrite(IN_1,HIGH);
+  digitalWrite(IN_2,HIGH);
+  digitalWrite(IN_3,LOW);
+  digitalWrite(IN_4,HIGH);
+  }
+void loop() {
+leftbackward();
+}
