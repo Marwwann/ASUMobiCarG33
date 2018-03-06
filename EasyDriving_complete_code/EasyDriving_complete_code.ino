108811f6b06b61209b36cb3bc7323e2bd78ede9e
Marwan Mohamed <Marwan.m9646@gmail.com>
2018-03-06 14:53:25 -0800

    Full Code

--- EasyDriving_complete_code/EasyDriving_complete_code.ino
+++ EasyDriving_complete_code/EasyDriving_complete_code.ino
@@ -3,15 +3,20 @@ int IN_1=4; //left motor
 int IN_2=5; //left motor
 int IN_3=6; //right motor
 int IN_4=7; //right motor
+int TriggerPin=13;
+int EchoPin=12;
+long duration , distance;
 void setup() {
  pinMode(IN_1,OUTPUT);
  pinMode(IN_2,OUTPUT);
  pinMode(IN_3,OUTPUT);
  pinMode(IN_4,OUTPUT);
+ pinMode(TriggerPin,OUTPUT);
+ pinMode(EchoPin,INPUT);
  Serial.begin(9600);
 }
 
-void forward()
+void left()
 {
   digitalWrite(IN_1,LOW);
   digitalWrite(IN_2,HIGH);
@@ -19,7 +24,7 @@ void forward()
   digitalWrite(IN_4,LOW);
   }
 
- void backward()
+ void right()
 {
   digitalWrite(IN_1,HIGH);
   digitalWrite(IN_2,LOW);
@@ -27,22 +32,49 @@ void forward()
   digitalWrite(IN_4,HIGH);
   }
 
-  void right()
+  void forward()
 {
-  digitalWrite(IN_1,LOW);
-  digitalWrite(IN_2,HIGH);
+  digitalWrite(IN_1,HIGH);
+  digitalWrite(IN_2,LOW);
+  digitalWrite(IN_3,HIGH);
+  digitalWrite(IN_4,LOW);
+  }
+   void rightforward()
+{
+  digitalWrite(IN_1,HIGH);
+  digitalWrite(IN_2,LOW);
   digitalWrite(IN_3,LOW);
   digitalWrite(IN_4,LOW);
   }
+   void rightbackward()
+{
+  digitalWrite(IN_1,LOW);
+  digitalWrite(IN_2,HIGH);
+  digitalWrite(IN_3,HIGH);
+  digitalWrite(IN_4,HIGH);
+  }
+  void backward()
+  {
+    digitalWrite(IN_1,LOW);
+    digitalWrite(IN_2,HIGH);
+    digitalWrite(IN_3,LOW);
+    digitalWrite(IN_4,HIGH);
+  }
 
-  void left()
+  void leftforward()
 {
   digitalWrite(IN_1,LOW);
   digitalWrite(IN_2,LOW);
   digitalWrite(IN_3,HIGH);
   digitalWrite(IN_4,LOW);
   }
-
+  void leftbackward()
+{
+  digitalWrite(IN_1,HIGH);
+  digitalWrite(IN_2,HIGH);
+  digitalWrite(IN_3,LOW);
+  digitalWrite(IN_4,HIGH);
+  }
    void Stop()
 {
   digitalWrite(IN_1,LOW);
@@ -55,19 +87,9 @@ void loop() {
   while(Serial.available()>0)
   {
     state=Serial.read();
-    if (state=='F')
+    if (state=='L')
     {
-      forward();
-      }
-
-      if (state=='B')
-    {
-      backward();
-      }
-
-      if (state=='L')
-    {
-      left();
+     left();
       }
 
       if (state=='R')
@@ -75,11 +97,48 @@ void loop() {
       right();
       }
 
+      if (state=='G')
+    {
+      leftforward();
+      }
+
+     if(state=='F')
+     {
+      forward();
+     }
+     if (state=='B')
+     {
+      backward();
+     }
+
       if (state=='S')
     {
        Stop();
       }
+      if (state=='J')
+      {
+        rightbackward();
+      }
+      if (state=='I')
+      {
+        rightforward();
+      }
+      if (state=='H')
+      {
+        leftbackward();
+      }
       
     }
+ digitalWrite(TriggerPin,LOW);
+ delayMicroseconds(2);
+ digitalWrite(TriggerPin,HIGH);
+ delayMicroseconds(10);
+ digitalWrite(TriggerPin,LOW);
+ duration= pulseIn(EchoPin,HIGH);
+ distance = duration*0.034/2;
+ if ( distance <= 10 )
+ {
+  Stop();
+ }
 
 }
