2c1441d7933a76f847ca82d8ed32f39ae993b74b
mariam-ayman <36281501+mariam-ayman@users.noreply.github.com>
2018-06-27 15:56:49 +0200

    Add files via upload

---
+++ phase_3_trial.ino
@@ -0,0 +1,125 @@
+ char state;
+ int sub; //length
+ char dir;  // forward or backward distance
+ String mode;
+ String submode;
+ String dist;
+ float distance;
+int IN_1=4; //left motor
+int IN_2=5; //left motor
+int IN_3=6; //right motor
+int IN_4=7; //right motor
+int pinA=11; //encoder pulses
+int initial; // initial state of encoder pin
+int pulses=0;
+void setup() {
+ pinMode(IN_1,OUTPUT);
+ pinMode(IN_2,OUTPUT);
+ pinMode(IN_3,OUTPUT);
+ pinMode(IN_4,OUTPUT);
+ pinMode(pinA,INPUT);
+ digitalRead(initial);
+ Serial.begin(9600);
+}
+
+void left()
+{
+  digitalWrite(IN_1,LOW);
+  digitalWrite(IN_2,HIGH);
+  digitalWrite(IN_3,HIGH);
+  digitalWrite(IN_4,LOW);
+  }
+
+ void right()
+{
+  digitalWrite(IN_1,HIGH);
+  digitalWrite(IN_2,LOW);
+  digitalWrite(IN_3,LOW);
+  digitalWrite(IN_4,HIGH);
+  }
+
+  void forward()
+{
+  digitalWrite(IN_1,HIGH);
+  digitalWrite(IN_2,LOW);
+  digitalWrite(IN_3,HIGH);
+  digitalWrite(IN_4,LOW);
+  }
+   void rightforward()
+{
+  digitalWrite(IN_1,HIGH);
+  digitalWrite(IN_2,LOW);
+  digitalWrite(IN_3,LOW);
+  digitalWrite(IN_4,LOW);
+  }
+   void rightbackward()
+{
+  digitalWrite(IN_1,LOW);
+  digitalWrite(IN_2,HIGH);
+  digitalWrite(IN_3,HIGH);
+  digitalWrite(IN_4,HIGH);
+  }
+  void backward()
+  {
+    digitalWrite(IN_1,LOW);
+    digitalWrite(IN_2,HIGH);
+    digitalWrite(IN_3,LOW);
+    digitalWrite(IN_4,HIGH);
+  }
+
+  void leftforward()
+{
+  digitalWrite(IN_1,LOW);
+  digitalWrite(IN_2,LOW);
+  digitalWrite(IN_3,HIGH);
+  digitalWrite(IN_4,LOW);
+  }
+  void leftbackward()
+{
+  digitalWrite(IN_1,HIGH);
+  digitalWrite(IN_2,HIGH);
+  digitalWrite(IN_3,LOW);
+  digitalWrite(IN_4,HIGH);
+  }
+   void Stop()
+{
+  digitalWrite(IN_1,LOW);
+  digitalWrite(IN_2,LOW);
+  digitalWrite(IN_3,LOW);
+  digitalWrite(IN_4,LOW);
+  }
+  
+void loop() {
+  while(Serial.available()>0)
+  {  
+     mode = Serial.read ();
+     submode= Serial.read ();   
+     if(mode=="accurate_driving")
+     {
+         if(submode=="shapes")
+          {
+              
+          }
+
+          else {
+            char dir = submode[0];
+            sub = submode.length();
+            for (int j=1; j<sub; j++)
+            {
+                 dist =              
+              
+             }
+             
+            
+            
+            }
+      
+              
+     }
+     
+ 
+      
+    }
+ 
+
+}
