364fcdea592a7368bef0f098e09cd3b2eda49b8c
MariamSafwat <36268243+MariamSafwat@users.noreply.github.com>
2018-06-27 20:57:30 +0200

    Add files via upload

---
+++ angle1.ino
@@ -0,0 +1,37 @@
+String angleName;
+float angle;
+int pin = 0;
+int Count;
+float PulseReq;
+
+void setup() {
+
+}
+
+void loop() {
+  
+  while(Serial.available()>0)
+  {
+    mode = Serial.read();
+    submode=Serial.read();
+    if(mode=="angle")
+    {
+      angleName = submode.substring(0);
+      angle = angleName.toFloat();
+
+      do
+      {
+        Count = attachInterrupt(digitalPinToInterrupt(pin),counting,RISING);
+        PulseReq = (Count * angle)/360;
+        digitalWrite(IN_1,HIGH);
+        digitalWrite(IN_2,LOW);
+        digitalWrite(IN_3,LOW);
+        digitalWrite(IN_4,LOW);
+      }
+      while(Count != PulseReq);
+    }
+
+    
+  }
+
+}
